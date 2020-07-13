2371d7e1fe317bba6c2dc6a3ad9559b264db70c9
GabrielBrotas <gabrielbrotas22@gmail.com>
2020-07-13 19:37:21 -0300

    notifications

--- functions/handlers/screams.js
+++ functions/handlers/screams.js
@@ -30,13 +30,18 @@ exports.postOneScream = (req, res) =>{
     const newScream = {
         body: req.body.body,
         userHandle: req.user.handle,
-        createdAt: new Date().toISOString()
+        userImage: req.user.imageUrl,
+        createdAt: new Date().toISOString(),
+        likeCount: 0,
+        commentCount: 0
     };
 
     db.collection('screams')
         .add(newScream)
         .then( (doc) => {
-            res.json({ message: `document ${doc.id} created successfully` })
+            const resScream = newScream;
+            resScream.screamId = doc.id;
+            res.json(resScream)
         })
         .catch( err => {
             res.status(500).json({error: 'something went wrong' + err})
@@ -78,7 +83,7 @@ exports.getScream = (req, res) => {
         })
 }
 
-// comment on a scream
+// comment on a scream, os comentarios vao ficar salvos em outra collection para tornar o app mais eficiente, caso seja um grande app com mais de 1k de comments para requisitar um post demoraria bastante e causaria mais por conta do trafego pelo request.
 exports.commentOnScream = (req, res) => {
     if(req.body.body.trim() === "") return res.status(400).json({error: "Must not be empty"})
 
@@ -95,6 +100,9 @@ exports.commentOnScream = (req, res) => {
             if(!doc.exists){
                 return res.status(404).json({error: 'Scream not found'})
             }
+            return doc.ref.update({commentCount: doc.data().commentCount + 1})
+        })
+        .then(() => {
             return db.collection('comments').add(newComment)
         })
         .then( () => {
@@ -103,5 +111,124 @@ exports.commentOnScream = (req, res) => {
         .catch( err => {
             console.log(err)
             res.status(500).json({error: "Something went wrong"})
+        })
+}
+
+// Like a scream, checar se o usuario já deu like e pegar a quantidadee, caso o post exista
+exports.likeScream = (req, res) => {
+    // pegar o documento que vai ser dado like
+    const likeDocument = db.collection('likes')
+        .where('userHandle', '==', req.user.handle)
+        .where('screamId', '==', req.params.screamId)
+        .limit(1);
+    
+    // vai pegar o documento do id passado
+    const screamDocument = db.doc(`/screams/${req.params.screamId}`);
+
+    let screamData = {};
+
+    screamDocument.get()
+        // doc vai ter todas as informações da scream passada
+        .then( doc => {
+            if(doc.exists){
+                // dados da scream
+                screamData = doc.data();
+                // colocar nos dados o id do documento que esta dando like
+                screamData.screamId = doc.id;
+                return likeDocument.get()
+            } else {
+                return res.status(404).json({error: 'Scream not found'})
+            }
+        })
+        .then( data => {
+            // se esses dados estiverem vazio
+            if(data.empty){
+                // adicionar na collection 'likes' um novo like com o id do post e o nome do user
+                return db.collection('likes').add({
+                    screamId: req.params.screamId,
+                    userHandle: req.user.handle
+                })
+                .then( () => {
+                    // aumentar o numero de likes do post
+                    screamData.likeCount++
+                    // atualizar o banco de dados real do firebase com o numero de likes
+                    return screamDocument.update({likeCount: screamData.likeCount});
+                })
+                .then( () => {
+                    return res.json(screamData)
+                })
+            } else {
+                return res.status(400).json({error: "scream already liked"})
+            }
+        })
+        .catch( err => {
+            console.error(err)
+            res.status(500).json({error: err.code})
+        })
+
+}
+
+exports.unlikeScream = (req, res) => {
+    const likeDocument = db.collection('likes')
+        .where('userHandle', '==', req.user.handle)
+        .where('screamId', '==', req.params.screamId)
+        .limit(1);
+    
+    const screamDocument = db.doc(`/screams/${req.params.screamId}`);
+
+    let screamData = {};
+
+    screamDocument.get()
+        .then( doc => {
+            if(doc.exists){
+                screamData = doc.data();
+                screamData.screamId = doc.id;
+                return likeDocument.get()
+            } else {
+                return res.status(404).json({error: 'Scream not found'})
+            }
+        })
+        .then( data => {
+            if(data.empty){
+                return res.status(400).json({error: "scream not liked"})
+            } else {
+                return db.doc(`likes/${data.docs[0].id}`).delete()
+                    .then( () => {
+                        screamData.likeCount--;
+                        return screamDocument.update({likeCount: screamData.likeCount})
+                    })
+                    .then( () => {
+                        res.json(screamData)
+                    })
+            }
+        })
+        .catch( err => {
+            console.error(err)
+            res.status(500).json({error: err.code})
+        })
+
+}
+
+// Delete a scream
+exports.deleteScream = (req, res) => {
+    const document = db.doc(`/screams/${req.params.screamId}`);
+
+    document.get()
+        .then( doc => {
+            if(!doc.exists){
+                return res.status(404).json({erro: "Scream not found"})
+            }
+            if(doc.data().userHandle !== req.user.handle) {
+                return res.status(403).json({error: "Unauthorized"})
+            } else {
+                return document.delete();
+            }
+        })
+        .then( () => {
+            res.json({message: "Scream deleted successfully"})
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(500).json({error: err.code})
         })
 }
--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,10 @@
 // libraries
 const app = require('express')();
 const functions = require('firebase-functions');
+const {db} = require('./util/admin')
 
 // routes
-const {getAllScreams, postOneScream, getScream, commentOnScream} = require('./handlers/screams');
+const {getAllScreams, postOneScream, getScream, commentOnScream, likeScream, unlikeScream, deleteScream} = require('./handlers/screams');
 const {signup, login, uploadImage, addUserDetails, getAuthenticatedUser} = require('./handlers/users')
 
 // helpers
@@ -17,9 +18,10 @@ const FirebaseAuth = require('./util/fbAuth')
     app.post('/scream', FirebaseAuth, postOneScream)
     app.get('/scream/:screamId', getScream);
 
-    //todo, delete scream
-    //todo, like a scream
-    //todo, unlike a scream
+    app.delete('/scream/:screamId', FirebaseAuth, deleteScream)
+    app.get('/scream/:screamId/like', FirebaseAuth, likeScream)
+    app.get('/scream/:screamId/unlike', FirebaseAuth, unlikeScream)
+
     app.post('/scream/:screamId/comment', FirebaseAuth, commentOnScream)
     
 // * Users routes
@@ -34,3 +36,76 @@ const FirebaseAuth = require('./util/fbAuth')
 // https://seusite.com/api/...
 // transformar as rotas app em https request no formato do firebase
 exports.api = functions.https.onRequest(app)
+
+// onCreate vai ativar um evento que nesse caso quando um novo like for criado no nosso database vai chamar essa função
+exports.createNotificationOnLike = functions.firestore.document('likes/{id}').onCreate( (snapshot) => {
+
+        db.doc(`/screams/${snapshot.data().screamId}`).get()
+            .then( doc => {
+                if(doc.exists){
+                    // criar um objeto com os dados da notificação para salvar e mandar para o recipient 
+                    return db.doc(`/notifications/${snapshot.id}`).set({
+                        createdAt: new Date().toISOString(),
+                        recipient: doc.data().userHandle,
+                        sender: snapshot.data().userHandle,
+                        type: 'like',
+                        read: false,
+                        screamId: doc.id
+                    });
+                }
+            })
+            // nao precisa retornar nada pois não faz parte da api
+            .then( () => {
+                return
+            })
+            .catch( err => {
+                console.error(err);
+                return;
+            })
+})
+
+// caso o usuario removar o like vai tirar a notificação
+exports.deleteNotificationOnUnlike = functions
+    .region('us-central1')
+    .firestore.document('likes/{id}')
+    .onDelete( (snapshot) => {
+        db.doc(`/notifications/${snapshot.id}`)
+            .delete()
+            .then( () => {
+                return
+            })
+            .catch( err =>{
+                console.error(err)
+                return
+            })
+})
+
+
+exports.createNotificationOnComment = functions
+    .region('us-central1')
+    .firestore.document('comments/{id}')
+    .onCreate( (snapshot) => {
+        db.doc(`/screams/${snapshot.data().screamId}`)
+            .get()
+            .then( doc => {
+                if(doc.exists){
+                    // criar um objeto com os dados da notificação para salvar e mandar para o recipient 
+                    return db.doc(`/notifications/${snapshot.id}`).set({
+                        createdAt: new Date().toISOString(),
+                        recipient: doc.data().userHandle,
+                        sender: snapshot.data().userHandle,
+                        type: 'comment',
+                        read: false,
+                        screamId: doc.id
+                    });
+                }
+            })
+            // nao precisa retornar nada pois não faz parte da api
+            .then( () => {
+                return
+            })
+            .catch( err => {
+                console.error(err);
+                return;
+            })
+})
